4323a260fc2a8db7f38eab9add151856376b8f40
hartgerv <hartgerveeman@gmail.com>
2015-04-19 00:24:31 +0200

    mobile ui fix

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 var myApp = angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
   'myApp.version',
   'mgcrea.ngStrap',
   'ui.sortable',
